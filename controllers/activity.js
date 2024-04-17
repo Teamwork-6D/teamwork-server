6c7c101779fff39cdeb9196c435bb407e047effc
eviedubs <118828635+eviedubs@users.noreply.github.com>
2024-04-17 09:40:09 +0100

    controllers - activites completed

--- controllers/activity.js
+++ controllers/activity.js
@@ -1,9 +1,31 @@
 import Activity from '../models/activity';
 
-export async function createActivity() {}
+export async function createActivity(activityData) {
+    const {type, projectId, message, task} = activityData;
 
-export async function updateActivity() {}
+    const newActivity = await Activity.create ({ type, projectId, message, task })
 
-export async function deleteActivity() {}
+    return newActivity;
+}
 
-export async function getAllProjectActivities() {}
+export async function updateActivity() {
+    const { _id, type, projectId, message, task} = activityData;
+
+    const activity = await Activity.findByIdAndUpdate(_id, { type, projectId, message, task });
+  
+    return activity;
+}
+
+export async function deleteActivity(activityData) {
+    const { _id } = activityData;
+  
+    const activity = await Activity.findByIdAndDelete(_id);
+  
+    return activity;
+}
+
+export async function getAllProjectActivities(projectId) {
+    const activities = await Activity.find({ projectId });
+
+    return activities;
+}
